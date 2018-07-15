1eae21d3be67019e2752b8e68196bc9eb1545d0f
Carlos Valderrama <cvalderramav@gmail.com>
2018-07-15 17:43:54 -0500

    adding html to m.a.

---
+++ src/main/java/com/earthteam/ocr/domain/Appointment.java
@@ -0,0 +1,12 @@
+package com.earthteam.ocr.domain;
+
+import java.time.LocalDate;
+
+public class Appointment {
+	private int appointmentId;
+	private Patient patient;
+	private Doctor doctor;
+	private int timespanId;
+	private LocalDate date;
+	
+}
---
+++ src/main/java/com/earthteam/ocr/domain/Patient.java
@@ -0,0 +1,14 @@
+package com.earthteam.ocr.domain;
+
+import java.time.LocalDate;
+
+public class Patient {
+	private int patientId;
+	private String firstName;
+	private String lastName;
+	private LocalDate dateOfBirth;
+	private String ssn;
+	private Address address;
+	private String userName;
+	private String password;
+}
